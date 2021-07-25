9647b7f00bda94e6f5bf2a573d79b04409452372
yonghun16 <yonghun16@naver.com>
2021-07-25 17:27:54 +0900

    javascript

---
+++ Javascript_life_coding/05-5.data_type.js
@@ -0,0 +1,23 @@
+/* 자료형 */
+let whatever = 'Mark';
+
+whatever= 37;
+console.log(typeof whatever);
+
+whatever = true;
+console.log(typeof whatever);
+console.log();
+
+// 데이터 타입
+// - 기본타입
+//   - Boolean, Null, Undefined
+//   - Number, String, Symbol
+// - 객체
+//   - 표준 내장 객체
+
+// Boolean
+const isTrue = true;
+const isFalse = false;
+console.log(isTrue, typeof isTrue);
+console.log(isFalse, typeof isFalse);
+
