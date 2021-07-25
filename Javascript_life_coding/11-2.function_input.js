298399d695d00e027396aec0be4eff92f449ae2b
yonghun16 <yonghun16@naver.com>
2021-07-25 14:23:00 +0900

    javasrcipt

--- Javascript_life_coding/11-2.function_input.js
+++ Javascript_life_coding/11-2.function_input.js
@@ -1,4 +1,5 @@
 /* 함수의 입력 */
+console.log(" 함수의 입력");
 function get_member1(){
     return 'egoing';
 }
@@ -18,3 +19,18 @@ function get_member3(){
     return 'sorialgi';
 }
 console.log(get_member3());
+
+
+/* 함수의 출력 */
+console.log(" 함수의 출력");
+function get_argument(arg){
+    return arg;
+}
+console.log(get_argument(1));
+console.log(get_argument(2));
+
+
+function get_argument2(arg1, arg2){
+    return arg1 + arg2
+}
+console.log(10, 20);
