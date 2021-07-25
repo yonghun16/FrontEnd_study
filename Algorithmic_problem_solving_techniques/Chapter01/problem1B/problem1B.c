6cc1fdf601918095f1dffd469fbe846527d7aaae
yonghun16 <yonghun16@naver.com>
2021-07-25 22:35:21 +0900

    배열의 최대값

--- Algorithmic_problem_solving_techniques/Chapter01/problem1B/problem1B.c
+++ Algorithmic_problem_solving_techniques/Chapter01/problem1B/problem1B.c
@@ -1,4 +1,4 @@
-// https://edu.goorm.io/learn/lecture/554/%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-%EB%AC%B8%EC%A0%9C%ED%95%B4%EA%B2%B0%EA%B8%B0%EB%B2%95-%EC%9E%85%EB%AC%B8/lesson/22753/%EB%AC%B8%EC%A0%9C1b-%EB%B0%B0%EC%97%B4%EC%9D%98-%EC%B5%9C%EB%8C%80%EA%B0%92
+// https://edu.goorm.io/learn/lecture/554/알고리즘-문제해결기법-입문/lesson/22753/문제1b-배열의-최대값
 
 #include <stdio.h>
 #include <stdlib.h>
