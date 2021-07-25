b4098571ed7cc18482abfd36c803c215050f15e6
yonghun16 <yonghun16@naver.com>
2021-07-25 21:34:56 +0900

    최대값 함수

---
+++ Algorithmic_problem_solving_techniques/Chapter01/problem1B/problem1B.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<iostream>
+
+using namespace std;
+
+/**
+ * * 배열의 최대값을 계산하는 함수.
+ * *
+ * * @param data
+ * * @param n
+ * * @return data[0] ~ data[n-1]중 최대값.
+ * */
+int getMax(int data[], int n)
+{
+    int answer = data[0];
+
+
+    return answer;
+}
+
+int main()
+{
+    int n;
+    int *data;
+
+    scanf("%d", &n);
+    data = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+            scanf("%d", &data[i]);
+        }
+
+    int answer = getMax(data, n);
+
+    printf("%d\n", answer);
+    delete[] data;
+    return 0;
+}
