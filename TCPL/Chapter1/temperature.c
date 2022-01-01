f82b8b7345adef78acfed6579d49c7828bef6949
yonghun16 <yonghun16@naver.com>
2022-01-02 00:52:00 +0900

    study

---
+++ TCPL/Chapter1/temperature.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+    float fahr, celsius;
+    float lower, upper, step;
+
+    lower = 0; /* lower limit of temperature scale */
+    upper = 300; /* upper limit */
+    step = 20; /* step size */
+
+    printf("  F    C\n");
+
+    fahr = lower;
+    while(fahr <= upper) {
+        celsius = (5.0 / 9.0) * (fahr - 32.0);
+        printf("%3.0f %6.1f\n", fahr, celsius);
+        fahr = fahr + step;
+    }
+    return 0;
+}
