e3b109eb288750ef2e68309defa867d35249e621
yonghun16 <yonghun16@naver.com>
2022-01-01 22:04:37 +0900

    study

---
+++ Python_study_alone/Chapter02/memo_2.py
@@ -0,0 +1,15 @@
+# +
+print("hello " + "sir")
+print("hello" + "!")
+
+#print("hello" + 1)  # error
+print("hello" + "1")
+
+# *
+print(3 * "hello ")
+
+# slising
+print("Hello"[0:3])
+
+# len()
+print(len("hello"))
