6fe17acd719bffb73917149aeec3e25647ae8c6a
yonghun16 <yonghun16@naver.com>
2022-01-02 22:12:17 +0900

    js

--- Javascript_study_alone/Chapter02/C2_momo2.js
+++
@@ -1,9 +0,0 @@
-// typeof(data)
-
-console.log(typeof('stringType'));
-console.log(typeof(123));
-console.log(typeof(true));
-
-console.log(typeof 'stringType');
-console.log(typeof 123);
-console.log(typeof true);
