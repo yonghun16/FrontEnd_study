074563203a53d950f89164dc1253758c8a9ee498
yonghun16 <yonghun16@naver.com>
2022-01-02 22:13:23 +0900

    study

---
+++ C_study_alone/Chapter02/2-5.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    // 세 가지 진법의 정수 상수
+    printf("%d\n", 12);
+    printf("%d\n", 014);
+    printf("%d\n", 0xc);
+
+    return 0;
+}
---
+++ C_study_alone/Chapter02/2-7.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // 문자와 문자열 데이터의 출력
+    printf("%c\n", 'A');
+    printf("%s\n", "A");
+    printf("%c는 %s입니다.\n", '1', "first");
+
+    return 0;
+}
