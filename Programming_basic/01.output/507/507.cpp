63bb1ae04e8f0d715f40140c98b32f01c6c37372
yonghun16 <yonghun16@naver.com>
2022-01-02 23:49:59 +0900

    507

--- Programming_basic/01.output/507/507.cpp
+++ Programming_basic/01.output/507/507.cpp
@@ -2,9 +2,11 @@
 
 using namespace std;
 
-int main()
+int main(void)
 {
     int n = 5;
     printf("%d Dan\n", n);
     printf("%d * 2 = %d", n, n*2);
+
+    return 0;
 }
--- Programming_basic/01.output/507/507.py
+++ Programming_basic/01.output/507/507.py
@@ -1,3 +1,3 @@
 n = 5
-print("%d Dan" % n)
-print(n + 2)
+print("{:d} Dan".format(n))
+print("{:d} * 2 = {:d}".format(n, n*2))
