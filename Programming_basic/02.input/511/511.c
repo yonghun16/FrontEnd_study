e4776dceb5ffd9ef2da6a3f342f2f26f1e4e5742
yonghun16 <yonghun16@naver.com>
2022-01-05 12:41:02 +0900

    511

---
+++ Programming_basic/02.input/511/511.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a = 55;
+    int b = 10;
+
+    printf("%d - %d = %d\n", a, b, a-b);
+
+    a = 2008; b = 1999;
+    printf("%d - %d = %d\n", a, b, a-b);
+    
+    return 0;
+}
