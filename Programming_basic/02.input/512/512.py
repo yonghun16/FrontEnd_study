03c570ed9d01ede29bce3516219b25092619d80e
yonghun16 <yonghun16@naver.com>
2022-01-05 15:43:01 +0900

    512

--- Programming_basic/02.input/512/512.py
+++ Programming_basic/02.input/512/512.py
@@ -1,4 +1,6 @@
 a = 49
 b = 0.2683
 
-print("{0:d} * {1:f} = {2:f}".format(a, b, a*b))
+print("%d * %.6lf = %.6lf" % (a, b, a*b))
+#print("{0:d} * {1:f} = {2:f}".format(a, b, a*b))
+#print(f'{a} * {b} = {a*b}')
