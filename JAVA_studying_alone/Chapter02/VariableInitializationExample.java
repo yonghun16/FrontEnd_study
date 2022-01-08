5878360d2d50549d8cd9558cb5e1666ab9515b5f
yonghun16 <yonghun16@naver.com>
2022-01-08 12:11:52 +0900

    java

---
+++ JAVA_studying_alone/Chapter02/VariableInitializationExample.java
@@ -0,0 +1,7 @@
+public class VariableInitializationExample {
+    public static void main(String[] args) {
+        int value;
+        int result = value + 10;
+        System.out.println(result);
+    }
+}
