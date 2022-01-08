01bf6e14a8015de8e843ce61c95e5a6d1fddd632
yonghun16 <yonghun16@naver.com>
2022-01-08 17:00:44 +0900

    esg

---
+++ studying_alone_of_Java/Chapter02/IntegerLiteralExample.java
@@ -0,0 +1,13 @@
+public class IntegerLiteralExample {
+    public static void main(String[] args) {
+        int var1 = 0b1011;
+        int var2 = 0206;
+        int var3 = 365;
+        int var4 = 0xB3;
+
+        System.out.println(var1);
+        System.out.println(var2);
+        System.out.println(var3);
+        System.out.println(var4);
+    }
+}
