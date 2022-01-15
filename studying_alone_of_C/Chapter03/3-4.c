385d45c8298c8e0995cc4e014d9f20338f2cdb99
yonghun16 <yonghun16@naver.com>
2022-01-15 15:35:05 +0900

    이름 바꿈:     1.5-01.c -> 1.5.1-01.c 	이름 바꿈:     1.5-02.c -> 1.5.1-02.c 	이름 바꿈:     1.5-03.c -> 1.5.2-01.c 	이름 바꿈:     1.5-04.c -> 1.5.2-02.c 	수정함:        ../../studying_alone_of_C/Chapter03/3-4.c

--- studying_alone_of_C/Chapter03/3-4.c
+++ studying_alone_of_C/Chapter03/3-4.c
@@ -7,6 +7,7 @@ int main(void)
 
     a = 4294967295;    // 큰 양수 저장
     printf("%d\n", a); // %d로 출력
+    printf("%u\n", a); // %d로 출력
     a = -1;            // 음수 저장
     printf("%u\n", a); // %u로 출력
 
